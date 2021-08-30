c396d022d39f34b1f638c7524ff44f5e63d99949
Demi Avaliani <demikoavaliani@gmail.com>
2021-08-30 17:21:16 +0300

    Added some CSS styles and component images

--- src/components/desktopApp/BoardGame.vue
+++ src/components/desktopApp/BoardGame.vue
@@ -1,12 +1,18 @@
 <template>
 	<div id="main-div">
-		<img src="@/assets/board-game/board-game-noframe.svg" usemap="#image-map" />
+		<div>
+			<img src="@/assets/board-game/board-game-noframe.svg" usemap="#image-map" />
 
-		<map name="image-map">
-			<area @click="numClick('14')" alt="14" href="#" coords="88,232,24" shape="circle" />
-			<area @click="numClick('13')" alt="13" href="#" coords="128,508,24" shape="circle" />
-			<area @click="numClick('15')" alt="15" href="#" coords="235,74,24" shape="circle" />
-		</map>
+			<map name="image-map">
+				<area @click="numClick('14')" alt="14" href="#" coords="88,232,24" shape="circle" />
+				<area @click="numClick('13')" alt="13" href="#" coords="128,508,24" shape="circle" />
+				<area @click="numClick('15')" alt="15" href="#" coords="235,74,24" shape="circle" />
+			</map>
+		</div>
+
+		<div class="menu-card">
+			<!-- <img src="@/assets/cards/character/baba_docka.png" /> -->
+		</div>
 	</div>
 </template>
 
@@ -29,12 +35,36 @@ export default {
 	mounted() {
 		ImageMap("img[usemap]");
 	},
+
+	beforeUpdate() {
+		ImageMap("img[usemap]");
+	},
 };
 </script>
 
 <style scoped>
+#main-div {
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	align-items: center;
+}
+
+.menu-card {
+	width: 300px;
+	height: 400px;
+	border: 1px solid #e4e4e4;
+	border-radius: 20px;
+	box-shadow: 0px 100px 80px rgba(205, 205, 205, 0.07),
+		0px 88.2875px 33.4221px rgba(205, 205, 205, 0.0503198),
+		0px 56.1188px 17.869px rgba(205, 205, 205, 0.0417275),
+		0px 30.0624px 10.0172px rgba(205, 205, 205, 0.035),
+		0px 13.7171px 5.32008px rgba(205, 205, 205, 0.0282725),
+		0px 4.3807px 2.21381px rgba(205, 205, 205, 0.0196802);
+}
+
 img {
-	display: block;
-	margin: auto;
+	width: 50vw;
 }
 </style>
