52006da26c44abedaa3a8685147e0b6345c8b920
Demi Avaliani <demikoavaliani@gmail.com>
2021-10-21 15:34:47 +0300

    Board Game 1) switched responsive img-map library 2) character movement logic fixed 3) added dynamic selection of card paths

--- src/components/desktopApp/BoardGame.vue
+++ src/components/desktopApp/BoardGame.vue
@@ -49,57 +49,58 @@
 					src="@/assets/board-game/board-1.svg"
 					class="board-game-img"
 					usemap="#image-map"
+					@load="getBoardPosition"
 				/>
 
 				<map name="image-map">
-					<area id="img-map-area" alt="101" href="" coords="513,682,19" shape="circle" />
-					<area id="img-map-area" alt="106" href="" coords="558,670,21" shape="circle" />
-					<area id="img-map-area" alt="107" href="" coords="607,691,20" shape="circle" />
-					<area id="img-map-area" alt="113" href="" coords="571,731,20" shape="circle" />
-					<area id="img-map-area" alt="114" href="" coords="545,775,19" shape="circle" />
-					<area id="img-map-area" alt="118" href="" coords="597,790,20" shape="circle" />
-					<area id="img-map-area" alt="119" href="" coords="642,764,20" shape="circle" />
-					<area id="img-map-area" alt="201" href="" coords="696,726,23" shape="circle" />
-					<area id="img-map-area" alt="215" href="" coords="702,676,20" shape="circle" />
-					<area id="img-map-area" alt="216" href="" coords="737,639,23" shape="circle" />
-					<area id="img-map-area" alt="217" href="" coords="795,654,22" shape="circle" />
-					<area id="img-map-area" alt="301" href="" coords="847,600,22" shape="circle" />
-					<area id="img-map-area" alt="314" href="" coords="828,545,20" shape="circle" />
-					<area id="img-map-area" alt="322" href="" coords="890,537,20" shape="circle" />
-					<area id="img-map-area" alt="330" href="" coords="863,485,20" shape="circle" />
-					<area id="img-map-area" alt="407" href="" coords="900,411,20" shape="circle" />
-					<area id="img-map-area" alt="408" href="" coords="860,376,20" shape="circle" />
-					<area id="img-map-area" alt="409" href="" coords="903,337,20" shape="circle" />
-					<area id="img-map-area" alt="410" href="" coords="857,306,22" shape="circle" />
-					<area id="img-map-area" alt="505" href="" coords="825,256,19" shape="circle" />
-					<area id="img-map-area" alt="506" href="" coords="779,228,21" shape="circle" />
-					<area id="img-map-area" alt="522" href="" coords="771,179,20" shape="circle" />
-					<area id="img-map-area" alt="523" href="" coords="723,162,20" shape="circle" />
-					<area id="img-map-area" alt="601" href="" coords="678,122,23" shape="circle" />
-					<area id="img-map-area" alt="602" href="" coords="625,142,21" shape="circle" />
-					<area id="img-map-area" alt="603" href="" coords="596,97,22" shape="circle" />
-					<area id="img-map-area" alt="604" href="" coords="543,121,23" shape="circle" />
-					<area id="img-map-area" alt="701" href="" coords="475,108,23" shape="circle" />
-					<area id="img-map-area" alt="707" href="" coords="425,148,20" shape="circle" />
-					<area id="img-map-area" alt="712" href="" coords="371,118,20" shape="circle" />
-					<area id="img-map-area" alt="802" href="" coords="309,155,21" shape="circle" />
-					<area id="img-map-area" alt="814" href="" coords="295,213,20" shape="circle" />
-					<area id="img-map-area" alt="819" href="" coords="240,225,20" shape="circle" />
-					<area id="img-map-area" alt="901" href="" coords="188,290,23" shape="circle" />
-					<area id="img-map-area" alt="927" href="" coords="210,336,20" shape="circle" />
-					<area id="img-map-area" alt="928" href="" coords="151,370,22" shape="circle" />
-					<area id="img-map-area" alt="1014" href="" coords="148,440,19" shape="circle" />
-					<area id="img-map-area" alt="1015" href="" coords="132,487,22" shape="circle" />
-					<area id="img-map-area" alt="1016" href="" coords="181,509,22" shape="circle" />
-					<area id="img-map-area" alt="1017" href="" coords="141,557,22" shape="circle" />
-					<area id="img-map-area" alt="1101" href="" coords="193,597,22" shape="circle" />
-					<area id="img-map-area" alt="1121" href="" coords="200,652,20" shape="circle" />
-					<area id="img-map-area" alt="1122" href="" coords="255,639,22" shape="circle" />
-					<area id="img-map-area" alt="1123" href="" coords="276,698,22" shape="circle" />
-					<area id="img-map-area" alt="1204" href="" coords="335,724,19" shape="circle" />
-					<area id="img-map-area" alt="1207" href="" coords="359,764,20" shape="circle" />
-					<area id="img-map-area" alt="1213" href="" coords="415,768,20" shape="circle" />
-					<area id="img-map-area" alt="1219" href="" coords="449,733,20" shape="circle" />
+					<area alt="101" href="" coords="513,682,19" shape="circle" />
+					<area alt="106" href="" coords="558,670,21" shape="circle" />
+					<area alt="107" href="" coords="607,691,20" shape="circle" />
+					<area alt="113" href="" coords="571,731,20" shape="circle" />
+					<area alt="114" href="" coords="545,775,19" shape="circle" />
+					<area alt="118" href="" coords="597,790,20" shape="circle" />
+					<area alt="119" href="" coords="642,764,20" shape="circle" />
+					<area alt="201" href="" coords="696,726,23" shape="circle" />
+					<area alt="215" href="" coords="702,676,20" shape="circle" />
+					<area alt="216" href="" coords="737,639,23" shape="circle" />
+					<area alt="217" href="" coords="795,654,22" shape="circle" />
+					<area alt="301" href="" coords="847,600,22" shape="circle" />
+					<area alt="314" href="" coords="828,545,20" shape="circle" />
+					<area alt="322" href="" coords="890,537,20" shape="circle" />
+					<area alt="330" href="" coords="863,485,20" shape="circle" />
+					<area alt="407" href="" coords="900,411,20" shape="circle" />
+					<area alt="408" href="" coords="860,376,20" shape="circle" />
+					<area alt="409" href="" coords="903,337,20" shape="circle" />
+					<area alt="410" href="" coords="857,306,22" shape="circle" />
+					<area alt="505" href="" coords="825,256,19" shape="circle" />
+					<area alt="506" href="" coords="779,228,21" shape="circle" />
+					<area alt="522" href="" coords="771,179,20" shape="circle" />
+					<area alt="523" href="" coords="723,162,20" shape="circle" />
+					<area alt="601" href="" coords="678,122,23" shape="circle" />
+					<area alt="602" href="" coords="625,142,21" shape="circle" />
+					<area alt="603" href="" coords="596,97,22" shape="circle" />
+					<area alt="604" href="" coords="543,121,23" shape="circle" />
+					<area alt="701" href="" coords="475,108,23" shape="circle" />
+					<area alt="707" href="" coords="425,148,20" shape="circle" />
+					<area alt="712" href="" coords="371,118,20" shape="circle" />
+					<area alt="802" href="" coords="309,155,21" shape="circle" />
+					<area alt="814" href="" coords="295,213,20" shape="circle" />
+					<area alt="819" href="" coords="240,225,20" shape="circle" />
+					<area alt="901" href="" coords="188,290,23" shape="circle" />
+					<area alt="927" href="" coords="210,336,20" shape="circle" />
+					<area alt="928" href="" coords="151,370,22" shape="circle" />
+					<area alt="1014" href="" coords="148,440,19" shape="circle" />
+					<area alt="1015" href="" coords="132,487,22" shape="circle" />
+					<area alt="1016" href="" coords="181,509,22" shape="circle" />
+					<area alt="1017" href="" coords="141,557,22" shape="circle" />
+					<area alt="1101" href="" coords="193,597,22" shape="circle" />
+					<area alt="1121" href="" coords="200,652,20" shape="circle" />
+					<area alt="1122" href="" coords="255,639,22" shape="circle" />
+					<area alt="1123" href="" coords="276,698,22" shape="circle" />
+					<area alt="1204" href="" coords="335,724,19" shape="circle" />
+					<area alt="1207" href="" coords="359,764,20" shape="circle" />
+					<area alt="1213" href="" coords="415,768,20" shape="circle" />
+					<area alt="1219" href="" coords="449,733,20" shape="circle" />
 				</map>
 			</div>
 
@@ -130,135 +131,27 @@
 								<img src="@/assets/board-game/info-sign.svg" width="18" />
 							</b-col>
 
-							<b-col cols="12" class="cards dishes pl-0 pr-4">
-								<b-row class="mx-auto">
-									<b-col cols="1" class="p-0 first-col">
-										<img src="@/assets/cards/dishes/borsh.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/cukerki.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/dishes/golubci.png" />
-									</b-col>
+							<b-col cols="12" class="pl-0 pr-4">
+								<b-row class="cards mx-auto">
+									<div class="p-0" style="width: 53px; height: 73px;" v-for="card in dishesDeck">
+										<img :src="require('@/assets/cards/dishes/' + card.name + '.png')" />
+									</div>
 								</b-row>
 							</b-col>
 
-							<b-col cols="12" class="cards stuff pl-0 pr-4">
-								<b-row class="mx-auto">
-									<b-col cols="1" class="p-0 first-col">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/stuff/bajan.png" />
-									</b-col>
+							<b-col cols="12" class="pl-0 pr-4">
+								<b-row class="cards mx-auto">
+									<div class="p-0" style="width: 53px; height: 73px;" v-for="card in ritualsDeck">
+										<img :src="require('@/assets/cards/rituals/' + card.name + '.png')" />
+									</div>
 								</b-row>
 							</b-col>
 
-							<b-col cols="12" class="cards rituals pl-0 pr-4">
-								<b-row class="mx-auto">
-									<b-col cols="1" class="p-0 first-col">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
-
-									<b-col cols="1" class="p-0">
-										<img src="@/assets/cards/rituals/chitannja.png" />
-									</b-col>
+							<b-col cols="12" class="pl-0 pr-4">
+								<b-row class="cards mx-auto">
+									<div class="p-0" style="width: 53px; height: 73px;" v-for="card in stuffDeck">
+										<img :src="require('@/assets/cards/stuff/' + card.name + '.png')" />
+									</div>
 								</b-row>
 							</b-col>
 						</b-row>
@@ -301,8 +194,9 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
 import ImageMap from "image-map";
+import * as imageMapResize from "@/plugins/imageMapResizer.min.js";
+import { mapState } from "vuex";
 import GameLogic from "./GameLogic";
 import ActiveRoomsGraph from "./ActiveRoomsGraph.vue";
 
@@ -316,12 +210,46 @@ export default {
 
 	data() {
 		return {
-			deskOrder: "",
+			deskOrder: "101",
 			boardX: "",
 			boardY: "",
 			adjustedCoordX: "",
 			adjustedCoordY: "",
 			diceBtnDisabled: "",
+			currentPlayerDeck: "",
+			deck: [],
+			dishesDeck: [
+				{ id: 15, name: "med", cardType: 1 },
+				{ id: 9, name: "polunica", cardType: 1 },
+				{ id: 13, name: "kartoplya", cardType: 1 },
+				{ id: 43, name: "uzvar", cardType: 1 },
+				{ id: 43, name: "uzvar", cardType: 1 },
+				{ id: 43, name: "uzvar", cardType: 1 },
+				{ id: 43, name: "uzvar", cardType: 1 },
+				{ id: 43, name: "uzvar", cardType: 1 },
+			],
+
+			ritualsDeck: [
+				{ id: 123, name: "chitannja", cardType: 2 },
+				{ id: 152, name: "prikrashati_hati", cardType: 2 },
+				{ id: 157, name: "kidannja_vinkiv", cardType: 2 },
+				{ id: 145, name: "obdaruvati", cardType: 2 },
+				{ id: 145, name: "obdaruvati", cardType: 2 },
+				{ id: 145, name: "obdaruvati", cardType: 2 },
+			],
+
+			stuffDeck: [
+				{ id: 193, name: "proskuri", cardType: 3 },
+				{ id: 187, name: "ptashki", cardType: 3 },
+				{ id: 228, name: "bandura", cardType: 3 },
+				{ id: 196, name: "pisachok", cardType: 3 },
+				{ id: 196, name: "pisachok", cardType: 3 },
+				{ id: 196, name: "pisachok", cardType: 3 },
+				{ id: 196, name: "pisachok", cardType: 3 },
+				{ id: 196, name: "pisachok", cardType: 3 },
+				{ id: 196, name: "pisachok", cardType: 3 },
+				{ id: 196, name: "pisachok", cardType: 3 },
+			],
 		};
 	},
 
@@ -329,6 +257,7 @@ export default {
 		...mapState({
 			game: "gameState",
 		}),
+
 		// diceDisabled() {
 		// 	this.$refs.gameLogic.throwDiceDisabled();
 		// },
@@ -362,33 +291,37 @@ export default {
 			this.boardY = board.y;
 			// console.log("Original boardX: ", this.boardX);
 			// console.log("Original boardY: ", this.boardY);
+
+			imageMapResize();
+
+			this.adjustImgMapCoords();
 		},
 
 		adjustImgMapCoords() {
-			setTimeout(() => {
-				let elem = document.querySelector(`area[alt='${this.deskOrder}']`);
-				// console.log("elem is: ", elem);
-				let coordsAttr = elem.getAttribute("coords");
-				// console.log("coords attribute is: ", coordsAttr);
-				let coordsAttrArray = coordsAttr.split(",");
-				// console.log("splitted attributes array:", coordsAttrArray);
-				this.adjustedCoordX = this.boardX + parseFloat(coordsAttrArray[0]);
-				this.adjustedCoordY = this.boardY + parseFloat(coordsAttrArray[1]);
-				// console.log("adjusted coordX: ", this.adjustedCoordX);
-				// console.log("adjusted coordY: ", this.adjustedCoordY);
-			});
+			let elem = document.querySelector(`area[alt='${this.deskOrder}']`);
+			// console.log("elem is: ", elem);
+			let coordsAttr = elem.getAttribute("coords");
+			// console.log("coords attribute is: ", coordsAttr);
+			let coordsAttrArray = coordsAttr.split(",");
+			// console.log("splitted attributes array:", coordsAttrArray);
+
+			this.adjustedCoordX = this.boardX + parseFloat(coordsAttrArray[0]);
+			this.adjustedCoordY = this.boardY + parseFloat(coordsAttrArray[1]);
+
+			// console.log("adjusted coordX: ", this.adjustedCoordX);
+			// console.log("adjusted coordY: ", this.adjustedCoordY);
+
+			this.udpatePlayerBoardPosition();
 		},
 
 		udpatePlayerBoardPosition() {
-			setTimeout(() => {
-				let player = document.getElementById("vukol-character");
+			let player = document.getElementById("vukol-character");
 
-				player.style.left = `${this.adjustedCoordX - 20}px`;
-				player.style.top = `${this.adjustedCoordY - 120}px`;
+			player.style.left = `${this.adjustedCoordX - 20}px`;
+			player.style.top = `${this.adjustedCoordY - 120}px`;
 
-				// console.log("player.style.left", player.style.left);
-				// console.log("player.style.top", player.style.top);
-			}, 15);
+			// console.log("player.style.left", player.style.left);
+			// console.log("player.style.top", player.style.top);
 		},
 
 		areaClick() {
@@ -406,29 +339,19 @@ export default {
 
 	watch: {
 		deskOrder() {
-			// this.getBoardPosition();
 			this.adjustImgMapCoords();
-			// this.udpatePlayerBoardPosition();
+		},
+
+		game() {
+			if (this.game && this.game.status === "STARTED" && this.game.step.currentPlayer) {
+				this.currentPlayerDeck = this.game.step.currentPlayer.deck;
+			}
 		},
 	},
 
 	mounted() {
-		ImageMap("img[usemap]", 0);
 		this.areaClick();
-		window.addEventListener("load", () => {
-			this.getBoardPosition();
-			this.adjustImgMapCoords();
-
-			this.$watch("deskOrder", function() {
-				console.log("WWWWWWWWWWWW");
-				this.udpatePlayerBoardPosition();
-			});
-		});
 	},
-
-	// beforeUpdate() {
-	// 	ImageMap("img[usemap]");
-	// },
 };
 </script>
 
@@ -535,58 +458,66 @@ p {
 .cards .p-0:hover img {
 	position: absolute;
 	width: 110px;
-	bottom: 20px;
+	bottom: 100px;
 	z-index: 9999;
 }
 
-/* .cards :nth-child(n + 1):hover img {
+.cards :nth-child(10) {
 	position: absolute;
-	bottom: 20px;
-	z-index: 9999;
-} */
+	top: 3px;
+	left: 108px;
+}
 
 .cards :nth-child(9) {
-	position: relative;
+	position: absolute;
 	top: 3px;
+	left: 96px;
 }
 
 .cards :nth-child(8) {
-	position: relative;
+	position: absolute;
 	top: 6px;
+	left: 84px;
 }
 
 .cards :nth-child(7) {
-	position: relative;
+	position: absolute;
 	top: 9px;
+	left: 72px;
 }
 
 .cards :nth-child(6) {
-	position: relative;
+	position: absolute;
 	top: 12px;
+	left: 60px;
 }
 
 .cards :nth-child(5) {
-	position: relative;
+	position: absolute;
 	top: 15px;
+	left: 48px;
 }
 
 .cards :nth-child(4) {
-	position: relative;
+	position: absolute;
 	top: 18px;
+	left: 36px;
 }
 
 .cards :nth-child(3) {
-	position: relative;
+	position: absolute;
 	top: 21px;
+	left: 24px;
 }
 
 .cards :nth-child(2) {
-	position: relative;
+	position: absolute;
 	top: 24px;
+	left: 12px;
 }
 
-.first-col {
-	position: relative;
+.cards > :nth-child(1) {
+	position: absolute;
 	top: 27px;
 }
 
