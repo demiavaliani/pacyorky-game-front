836b933bd0568876351dc2db189fe5d9c0f8e119
Demi Avaliani <demikoavaliani@gmail.com>
2021-12-01 14:12:29 +0200

    fixes

--- src/components/desktopApp/BoardGame.vue
+++ src/components/desktopApp/BoardGame.vue
@@ -451,7 +451,7 @@
 						</p>
 					</b-col>
 
-					<b-col cols="3" class="my-2" v-for="player in playersInGame" :key="player.id">
+					<b-col cols="3" class="my-2" v-for="player in game.players" :key="player.id">
 						<img :src="require('@/assets/cards/character/' + player.character.name + '.png')" />
 						<p style="font-size: 26px">
 							{{ player.character.name }}
