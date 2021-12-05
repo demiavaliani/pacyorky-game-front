ee7b62e951244fe6f5872a0a8ed09f2285a445d2
Demi Avaliani <demikoavaliani@gmail.com>
2021-12-05 02:20:05 +0200

    PGF-109 1) previous layout was broken. changed to a new one 2) fonts are now preloaded at app entry point

--- src/App.vue
+++ src/App.vue
@@ -67,37 +67,37 @@ export default {
 
 @font-face {
 	font-family: "Montserrat";
-	src: url("assets/fonts/Montserrat-Light.ttf") format("truetype");
+	src: url("../public/fonts/Montserrat-Light.ttf") format("truetype");
 	font-weight: 300;
 }
 
 @font-face {
 	font-family: "Montserrat";
-	src: url("assets/fonts/Montserrat-Regular.ttf") format("truetype");
+	src: url("../public/fonts/Montserrat-Regular.ttf") format("truetype");
 	font-weight: 400;
 }
 
 @font-face {
 	font-family: "Montserrat";
-	src: url("assets/fonts/Montserrat-Medium.ttf") format("truetype");
+	src: url("../public/fonts/Montserrat-Medium.ttf") format("truetype");
 	font-weight: 500;
 }
 
 @font-face {
 	font-family: "Montserrat";
-	src: url("assets/fonts/Montserrat-SemiBold.ttf") format("truetype");
+	src: url("../public/fonts/Montserrat-SemiBold.ttf") format("truetype");
 	font-weight: 600;
 }
 
 @font-face {
 	font-family: "Montserrat";
-	src: url("assets/fonts/Montserrat-Bold.ttf") format("truetype");
+	src: url("../public/fonts/Montserrat-Bold.ttf") format("truetype");
 	font-weight: 700;
 }
 
 @font-face {
 	font-family: "Amatic_SC";
-	src: url("assets/fonts/AmaticSC-Bold.ttf") format("truetype");
+	src: url("../public/fonts/AmaticSC-Bold.ttf") format("truetype");
 	font-weight: bold;
 }
 </style>
